149b94f517c4567b9f377d27ed58474a94bc0510
Mingfei Li <ml@mingfei.io>
2024-06-01 22:20:45 -0700

    Experimenting with wider MLP

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 class Config():
-    num_episodes_train = 5000
-    max_eps = 0.9
-    min_eps = 0.9
+    num_episodes_train = 3000
+    max_eps = 0.05
+    min_eps = 0.05
     n_eps = 50_000
     max_lr = 1e-4
     min_lr = 1e-4
     n_lr = 500_000
     target_update_freq = 50
-    batch_size = 96
+    batch_size = 128
     gamma = 0.99
     learning_start = 10_000
     buffer_size = 50_000
     grad_clip = 1e4
-    exp_id = "batchsize=96,lr=1e-4,layer=6,eps=0.9,num_episodes_train=5000"
+    exp_id = "batchsize=128,lr=1e-4,width=128,num_episodes_train=5000"
